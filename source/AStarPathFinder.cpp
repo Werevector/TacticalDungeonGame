bf99dd5a60627a93b94bf705c315619c58b446ce
Runar Tistel <runartistel@gmail.com>
2016-07-17 00:10:33 +0200

    a

--- source/AStarPathFinder.cpp
+++ source/AStarPathFinder.cpp
@@ -1,12 +1,15 @@
 #include "AStarPathFinder.h"
 
+#define STRAIGHT_COST 10
+#define DIAG_COST 14
+
 bool AStarPathFinder::FindPath(utility::Point2d position, utility::Point2d destination)
 {
 	bool pathFound = false;
 
 	if(mMapPtr != NULL)
 	{
-
+		pathFound = _FindPath(position, destination);
 	}
 	return pathFound;
 }
@@ -14,4 +17,30 @@ bool AStarPathFinder::FindPath(utility::Point2d position, utility::Point2d desti
 void AStarPathFinder::SetMap(GameMap * mapPtr)
 {
 	mMapPtr = mapPtr;
+}
+
+bool AStarPathFinder::_FindPath(utility::Point2d position, utility::Point2d destination)
+{
+	bool pathFound = false;
+
+	std::list<ASTSquare> openlist;
+	std::list<ASTSquare> closedlist;
+
+
+	
+	return pathFound;
+}
+
+int AStarPathFinder::Heuristic(utility::Point2d position, utility::Point2d destination)
+{
+	//Manhattan Heuristic
+	int x = abs(floor(destination.x - position.x));
+	int y = abs(floor(destination.y - position.y));
+
+	return x + y;
+}
+
+void AStarPathFinder::AddAdjacent(std::list<ASTSquare>& sqList, utility::Point2d point)
+{
+
 }
--- source/AStarPathFinder.h
+++ source/AStarPathFinder.h
@@ -2,18 +2,39 @@
 #include "Utils.h"
 #include "GameMap.h"
 #include <memory>
+#include <list>
+
+class ASTSquare {
+public:
+	ASTSquare() {}
+	ASTSquare(int g) : G(g) {}
+	ASTSquare(int g, int h) : G(g), H(h) {}
+	ASTSquare(int g, int h, int f) : G(g), H(h), F(f) {}
+
+	ASTSquare* parent;
+
+	int G;
+	int H;
+	int F;
+};
 
 class AStarPathFinder {
 	
 public:
 
 	AStarPathFinder() {}
+	AStarPathFinder(GameMap* map) { SetMap(map); }
+	
 	bool FindPath(utility::Point2d position ,utility::Point2d destination);
 	void SetMap(GameMap* mapPtr);
 
 private:
 
+	bool _FindPath(utility::Point2d position, utility::Point2d destination);
+	int Heuristic(utility::Point2d position, utility::Point2d destination);
+	void AddAdjacent(std::list<ASTSquare>& sqList, utility::Point2d point);
 	GameMap* mMapPtr;
 	utility::Point2d mCurrentDestination;
+
 
 };
